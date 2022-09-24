024f972373bc8e8613910a150c29ec9ca6f84531
omar-ez95 <omar.eddin@code.berlin>
2022-09-24 20:27:30 +0200

    updated database

--- carbonized/settings.py
+++ carbonized/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'carbonized.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres', 
+        'PASSWORD': 'carbonized',
+        'HOST': 'database-2.ct17kp1mrf2z.eu-central-1.rds.amazonaws.com', 
+        'PORT': '5432',
     }
 }
 
