a6da87f3cca9e4789083148bd87ff8fa384d6774
omar-ez95 <omar.eddin@code.berlin>
2022-09-24 21:00:12 +0200

    n

--- carbonized/settings.py
+++ carbonized/settings.py
@@ -102,14 +102,19 @@ WSGI_APPLICATION = 'carbonized.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres', 
+        'NAME': '', 
         'USER': 'postgres', 
         'PASSWORD': 'carbonized',
         'HOST': 'database-2.ct17kp1mrf2z.eu-central-1.rds.amazonaws.com', 
         'PORT': '5432',
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
