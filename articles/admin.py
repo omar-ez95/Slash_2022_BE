0807c22fd1ca81569eedcd16d33b6349cf8774f3
omar-ez95 <omar.eddin@code.berlin>
2022-09-25 02:41:42 +0200

    s

--- articles/admin.py
+++ articles/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from articles.models import Article
+from articles.models import Article, Discource
 
 # Register your models here.
 admin.site.register(Article)
+admin.site.register(Discource)
+
--- articles/models.py
+++ articles/models.py
@@ -22,4 +22,13 @@ class Article(models.Model):
     def __str__(self):
         return self.name
 
+class Discource(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, null=True, blank=True)
+    describtion = models.TextField(max_length=150, null=True, blank=True)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
 
